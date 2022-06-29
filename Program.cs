04d8b5477313d917c85d6a4ce4f86be4d3052ad3
amerh12 <91185105+amerh12@users.noreply.github.com>
2022-06-29 18:56:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga19Vaje2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Vsota(5, 15));
+
+        }
+
+        static int Vsota(int num1, int num2)
+        {
+
+            int sestevek = 0;
+
+            for (; num1 < num2; num1++)
+            {
+                sestevek += num1;
+            }
+
+
+
+            return sestevek;
+        }
+    }
+}
+   
